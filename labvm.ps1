90ffa9eeb7b965ed76b1b1debf2efe8c705b5a59
hiryamada <71864722+hiryamada@users.noreply.github.com>
2021-06-28 16:44:11 +0900

    Update labvm.ps1

--- labvm.ps1
+++ labvm.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install packages
-choco install -y microsoft-edge azure-cli az.powershell vscode dotnetcore-sdk microsoftazurestorageexplorer setdefaultbrowser 
+choco install -y microsoft-edge azure-cli az.powershell vscode dotnetcore-sdk microsoftazurestorageexplorer setdefaultbrowser azure-functions-core-tools
 
 # install httprepl
 & "C:\Program Files\dotnet\dotnet.exe" tool install -g Microsoft.dotnet-httprepl
