8abb4d72d322b73d0bed9957ce79516b80e34113
Masato Kikukawa <kikukawa9@gmail.com>
2021-12-24 00:43:06 +0900

    Set VS2019 again

--- labvm.ps1
+++ labvm.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install packages
-choco install -y microsoft-edge azure-cli az.powershell vscode dotnetcore-sdk microsoftazurestorageexplorer setdefaultbrowser azure-functions-core-tools
+choco install -y microsoft-edge azure-cli az.powershell vscode dotnetcore-sdk microsoftazurestorageexplorer setdefaultbrowser azure-functions-core-tools visualstudio2019community git
 
 # install httprepl
 & "C:\Program Files\dotnet\dotnet.exe" tool install -g Microsoft.dotnet-httprepl
